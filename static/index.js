63ebc7a75cde25a95beedba63758b085e2b5e622
Derek Lorenzo <87837145+DerekLorenzo@users.noreply.github.com>
2023-05-11 19:23:44 -0400

    mod link wheel

--- static/index.js
+++ static/index.js
@@ -1,84 +1,19 @@
-const links = [
-    {
-      title: "Guqin",
-      image: "images/guqin.jpg",
-      link: "guqin.html"
-    },
-    {
-      title: "Bells",
-      image: "images/bells.jpg",
-      link: "bells.html"
-    },
-    {
-      title: "Pipa",
-      image: "images/guqin.jpg",
-      link: "pipa.html"
-    },
-    {
-      title: "Dizi",
-      image: "images/bells.jpg",
-      link: "dizi.html"
-    },
-    {
-      title: "Placeholder",
-      image: "images/guqin.jpg",
-      link: "guqin.html"
-    }
-];
+const links = document.querySelectorAll('.link');
+let centerLink = Math.floor(links.length / 2);
 
-const wheel = document.querySelector(".wheel");
-const totalLinks = links.length;
-const degrees = 360 / totalLinks;
-let currentRotation = 0;
-
-links.forEach((link, index) => {
-  const linkElement = document.createElement("a");
-  linkElement.classList.add("link");
-  linkElement.style.transform = `rotate(${degrees * index}deg)`;
-
-  const imgElement = document.createElement("img");
-  imgElement.src = link.image;
-  imgElement.classList.add("link-image");
-
-  const titleElement = document.createElement("p");
-  titleElement.classList.add("link-title");
-  titleElement.innerText = link.title;
-
-  linkElement.appendChild(imgElement);
-  linkElement.appendChild(titleElement);
-  wheel.appendChild(linkElement);
-});
-
-function rotateWheel(deg) {
-  currentRotation += deg;
-  wheel.style.transform = `rotate(${currentRotation}deg)`;
-
-  const links = document.querySelectorAll(".link");
-  links.forEach((link) => {
-    const linkRect = link.getBoundingClientRect();
-    const wheelRect = wheel.getBoundingClientRect();
-    const centerX = wheelRect.left + wheelRect.width / 2;
-    const linkCenterX = linkRect.left + linkRect.width / 2;
-    const distanceFromCenter = Math.abs(centerX - linkCenterX);
-    const maxDistanceFromCenter = wheelRect.width / 2 - linkRect.width / 2;
-    const distanceRatio = distanceFromCenter / maxDistanceFromCenter;
-    const scale = 1 - distanceRatio * 0.5;
-    link.style.transform = `scale(${scale})`;
-  });
+function setCenterLink(index) {
+  links[centerLink].classList.remove('center');
+  centerLink = index;
+  links[centerLink].classList.add('center');
 }
 
-document.addEventListener("mousemove", (event) => {
-  const wheelRect = wheel.getBoundingClientRect();
-  const centerX = wheelRect.left + wheelRect.width / 2;
-  const mouseY = event.clientY;
-  
-  const degPerPixel = 360 / wheelRect.height;
-  const degToRotate = (mouseY - centerX) * degPerPixel;
-  
-  rotateWheel(degToRotate);
-});
+function moveLinks(event) {
+  const direction = event.deltaY > 0 ? 1 : -1;
+  const newCenterLink = centerLink + direction;
+  if (newCenterLink >= 0 && newCenterLink < links.length) {
+    setCenterLink(newCenterLink);
+  }
+}
 
-document.addEventListener("mouseleave", () => {
-  rotateWheel(-currentRotation);
-  currentRotation = 0;
-});
+links[centerLink].classList.add('center');
+document.querySelector('.scroller').addEventListener('wheel', moveLinks);
