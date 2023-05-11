a7a173c28a02f6c9e5ee5889bc730eea5113f20f
Derek Lorenzo <87837145+DerekLorenzo@users.noreply.github.com>
2023-05-11 01:11:50 -0400

    remove unneeded code

--- static/bells.js
+++
@@ -1,5 +0,0 @@
-// Add a click event listener to flip the card on click
-var flipCard = document.querySelector('.flip-card');
-flipCard.addEventListener('click', function() {
-  this.classList.toggle('flip');
-});
