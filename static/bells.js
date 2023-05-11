6c97973bcc88005130ff0fbd5c63fd92dd1beb85
Derek Lorenzo <87837145+DerekLorenzo@users.noreply.github.com>
2023-05-11 02:27:09 -0400

    updating interactive bell

--- static/bells.js
+++ static/bells.js
@@ -1,21 +1,29 @@
-const areas = document.querySelectorAll('area');
-const audio = document.getElementById('audio');
-const indicators = document.querySelectorAll('.indicator');
+const bellAreas = document.querySelectorAll('.bell-area');
+const bellIndicators = [];
 
-areas.forEach((area, index) => {
-  area.addEventListener('click', () => {
-    audio.src = area.dataset.sound;
-    audio.play();
-    activateIndicator(index);
-  });
-});
+for (let i = 0; i < bellAreas.length; i++) {
+  const area = bellAreas[i];
+  const coords = area.coords.split(',').map(coord => parseInt(coord));
+  const x = coords.filter((coord, index) => index % 2 === 0);
+  const y = coords.filter((coord, index) => index % 2 !== 0);
+  const minX = Math.min(...x);
+  const minY = Math.min(...y);
+  const maxX = Math.max(...x);
+  const maxY = Math.max(...y);
+  const centerX = (minX + maxX) / 2;
+  const centerY = (minY + maxY) / 2;
 
-function activateIndicator(index) {
-  indicators.forEach((indicator, i) => {
-    if (i === index) {
-      indicator.classList.add('active');
-    } else {
-      indicator.classList.remove('active');
-    }
+  const indicator = document.createElement('div');
+  indicator.classList.add('bell-indicator');
+  indicator.style.left = `${centerX}px`;
+  indicator.style.top = `${centerY}px`;
+  area.parentNode.appendChild(indicator);
+  bellIndicators.push(indicator);
+
+  area.addEventListener('click', event => {
+    const soundId = event.target.dataset.sound;
+    const sound = document.getElementById(soundId);
+    sound.currentTime = 0;
+    sound.play();
   });
 }
