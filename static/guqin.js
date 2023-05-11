e58ddd7df749f5ca5482e5fd3489e71b54991bbc
Derek Lorenzo <87837145+DerekLorenzo@users.noreply.github.com>
2023-05-11 12:24:44 -0400

    Reduced code for timeline

--- static/guqin.js
+++
@@ -1,8 +0,0 @@
-const events = document.querySelectorAll('.event');
-
-events.forEach(event => {
-  event.addEventListener('click', () => {
-    // Do something when event is clicked
-    console.log(`Event ${event.id} clicked`);
-  });
-});
