cdda2ac734e3bbb4ef4c83e52e92b62e31a4fc2b
Derek Lorenzo <87837145+DerekLorenzo@users.noreply.github.com>
2023-05-11 11:13:27 -0400

    Update bells.js

--- static/bells.js
+++ static/bells.js
@@ -6,14 +6,8 @@ console.log(bellAreas.length)
 for (let i = 0; i < bellAreas.length; i++) {
   const area = bellAreas[i];
   const coords = area.coords.split(',').map(coord => parseInt(coord));
-  const x = coords.filter((coord, index) => index % 2 === 0);
-  const y = coords.filter((coord, index) => index % 2 !== 0);
-  const minX = Math.min(...x);
-  const minY = Math.min(...y);
-  const maxX = Math.max(...x);
-  const maxY = Math.max(...y);
-  const centerX = (minX + maxX) / 2;
-  const centerY = (minY + maxY) / 2;
+  const x = coords[0];
+  const y = coords[1];
 
   const indicator = document.createElement('div');
   indicator.classList.add('bell-indicator');
