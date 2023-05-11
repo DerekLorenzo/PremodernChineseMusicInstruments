3a9b3d56babd76fdda771fb690abd87e5ad8e88c
Derek Lorenzo <87837145+DerekLorenzo@users.noreply.github.com>
2023-05-11 18:41:23 -0400

    Update index.js

--- static/index.js
+++ static/index.js
@@ -31,25 +31,57 @@ const totalLinks = links.length;
 const degrees = 360 / totalLinks;
 
 links.forEach((link, index) => {
-    const linkElement = document.createElement("div");
-    linkElement.classList.add("link");
-    linkElement.style.transform = `rotate(${degrees * index}deg)`;
+  const linkElement = document.createElement("a");
+  linkElement.classList.add("link");
+  linkElement.style.transform = `rotate(${degrees * index}deg)`;
 
-    const imgElement = document.createElement("img");
-    imgElement.src = link.image;
-    const titleElement = document.createElement("p");
-    titleElement.classList.add("link-title");
-    titleElement.innerText = link.title;
+  const imgElement = document.createElement("img");
+  imgElement.src = link.image;
 
-    linkElement.appendChild(imgElement);
-    linkElement.appendChild(titleElement);
-    wheel.appendChild(linkElement);
+  const titleElement = document.createElement("p");
+  titleElement.classList.add("link-title");
+  titleElement.innerText = link.title;
 
-    linkElement.addEventListener("click", () => {
-        const activeLink = document.querySelector(".link.active");
-        if (activeLink) {
-            activeLink.classList.remove("active");
-        }
-        linkElement.classList.add("active");
-    });
+  linkElement.appendChild(imgElement);
+  linkElement.appendChild(titleElement);
+  wheel.appendChild(linkElement);
+
+  linkElement.addEventListener("click", () => {
+    const activeLink = document.querySelector(".link.active");
+    if (activeLink) {
+      activeLink.classList.remove("active");
+    }
+    linkElement.classList.add("active");
+  });
 });
+
+let activeLinkIndex = 0;
+
+function updateWheel() {
+  const currentActiveLink = document.querySelector(".link.active");
+  if (currentActiveLink) {
+    currentActiveLinkIndex = Array.from(wheel.children).indexOf(currentActiveLink);
+    activeLinkIndex = currentActiveLinkIndex;
+  }
+
+  const nextActiveLinkIndex = (activeLinkIndex + 1) % totalLinks;
+  const nextActiveLink = wheel.children[nextActiveLinkIndex];
+
+  for (let i = 0; i < totalLinks; i++) {
+    const link = wheel.children[i];
+    const linkRotation = link.style.transform.replace("rotate(", "").replace("deg)", "");
+    const newRotation = parseInt(linkRotation) - degrees;
+
+    link.style.transform = `rotate(${newRotation}deg)`;
+
+    if (link === nextActiveLink) {
+      link.classList.add("active");
+    } else {
+      link.classList.remove("active");
+    }
+  }
+
+  activeLinkIndex = nextActiveLinkIndex;
+}
+
+setInterval(updateWheel, 3000);
