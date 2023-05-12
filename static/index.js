3005075e73ea47916b46fd92172d3e0081f7271b
Derek Lorenzo <87837145+DerekLorenzo@users.noreply.github.com>
2023-05-11 21:49:27 -0400

    Update index.js

--- static/index.js
+++ static/index.js
@@ -1,12 +1,12 @@
 const links = document.querySelectorAll('.link');
 
 links.forEach((link) => {
-    const title = link.querySelector('.link-title');
-    const image = link.querySelector('.link-image');
+  const title = link.querySelector('.link-title');
+  const image = link.querySelector('.link-image');
 
-    image.addEventListener('load', () => {
-        title.style.top = `${image.offsetHeight - title.offsetHeight}px`;
-    });
+  image.addEventListener('load', () => {
+    title.style.top = `${image.offsetHeight - title.offsetHeight}px`;
+  });
 });
 
   
