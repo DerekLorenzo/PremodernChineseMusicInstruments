52547b6e169add3ee17958d901ee2d6a2194b8ba
Derek Lorenzo <87837145+DerekLorenzo@users.noreply.github.com>
2023-05-11 21:07:08 -0400

    functionality fix and adding images

--- static/index.js
+++ static/index.js
@@ -1,29 +1,8 @@
-const links = document.querySelectorAll('.link');
-let centerLink = Math.floor(links.length / 2);
-
-function setCenterLink(index) {
-  links[centerLink].classList.remove('center');
-  centerLink = index;
-  links[centerLink].classList.add('center');
-}
-
-function moveLinks(event) {
-  const containerWidth = event.currentTarget.offsetWidth;
-  const mousePosition = event.clientX - event.currentTarget.offsetLeft;
-  const linkWidth = links[0].offsetWidth;
-  const linkCenter = linkWidth / 2;
-
-  let newCenterLink = centerLink;
-  if (mousePosition < centerLink * linkWidth + linkCenter) {
-    newCenterLink--;
-  } else if (mousePosition > centerLink * linkWidth + linkCenter) {
-    newCenterLink++;
-  }
-
-  if (newCenterLink >= 0 && newCenterLink < links.length) {
-    setCenterLink(newCenterLink);
-  }
-}
-
-links[centerLink].classList.add('center');
-document.querySelector('.container').addEventListener('mousemove', moveLinks);
+document.querySelectorAll('.link').forEach(link => {
+    link.addEventListener('click', (event) => {
+      event.preventDefault();
+      const url = link.dataset.url;
+      window.location.href = url;
+    });
+  });
+  
