c19e19297371b6194c4d684190123e494de0ccca
Derek Lorenzo <87837145+DerekLorenzo@users.noreply.github.com>
2023-05-11 22:13:13 -0400

    Update interactive bell

--- static/bells.js
+++ static/bells.js
@@ -1,56 +1,21 @@
-console.log("loaded bells.js")
-const bellAreas = document.querySelectorAll('.bell-area');
-const bellIndicators = [];
-console.log(bellAreas.length)
+// Load the audio files
+var sideSound = new Audio('sounds/bells-1.mp3');
+var centerSound = new Audio('sounds/bells-2.mp3');
 
-window.onload = function () {
-    var ImageMap = function (map, img) {
-        var n,
-            areas = map.getElementsByTagName('area'),
-            len = areas.length,
-            coords = [],
-            previousWidth = 550;
-        for (n = 0; n < len; n++) {
-            coords[n] = areas[n].coords.split(',');
-        }
-        this.resize = function () {
-            var n, m, clen,
-                x = img.offsetWidth / previousWidth;
-            for (n = 0; n < len; n++) {
-                clen = coords[n].length;
-                for (m = 0; m < clen; m++) {
-                    coords[n][m] *= x;
-                }
-                areas[n].coords = coords[n].join(',');
-            }
-            previousWidth = document.body.clientWidth;
-            console.log("resized")
-            return true;
-        };
-        window.onresize = this.resize;
-    },
-    imageMap = new ImageMap(document.getElementById('map-id'), document.getElementById('interactive-bell'));
-    imageMap.resize();
-}
+// Get the bell image and clickable areas
+var bell = document.getElementById('bell');
+var sideAreas = document.querySelectorAll('.side-area');
+var centerArea = document.querySelector('.center-area');
 
-for (let i = 0; i < bellAreas.length; i++) {
-  const area = bellAreas[i];
-  const coords = area.coords.split(',').map(coord => parseInt(coord));
-  const x = coords[0];
-  const y = coords[1];
-
-  const indicator = document.createElement('div');
-  indicator.classList.add('bell-indicator');
-  indicator.style.left = `${x}px`;
-  indicator.style.top = `${y}px`;
-  area.parentNode.appendChild(indicator);
-  bellIndicators.push(indicator);
-
-  area.addEventListener('click', event => {
-    const soundId = event.target.dataset.sound;
-    const sound = document.getElementById(soundId);
-    sound.currentTime = 0;
-    sound.play();
+// Add click event listeners to the clickable areas
+sideAreas.forEach(function (area) {
+  area.addEventListener('click', function () {
+    // Play the side bell sound
+    sideSound.play();
   });
-  console.log("done")
-}
+});
+
+centerArea.addEventListener('click', function () {
+  // Play the center bell sound
+  centerSound.play();
+});
