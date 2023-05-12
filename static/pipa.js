afa054f3a5aa877388910d7db2a27dad07d3ebbb
Derek Lorenzo <87837145+DerekLorenzo@users.noreply.github.com>
2023-05-12 02:02:17 -0400

    update interactive labels

--- static/pipa.js
+++ static/pipa.js
@@ -1,20 +1,12 @@
-// Get the label and overlay elements
-const label1 = document.getElementById('label1');
-const label2 = document.getElementById('label2');
-const overlay = document.getElementById('overlay');
+// Change these variables to match your label IDs and text
+const label1 = document.getElementById('label-1');
+const label2 = document.getElementById('label-2');
 
-// Add event listeners to the overlay
-overlay.addEventListener('mouseover', () => {
-  // Show the appropriate label based on which part of the overlay was clicked
-  if (event.offsetX < overlay.offsetWidth / 2) {
-    label1.classList.add('active');
-  } else {
-    label2.classList.add('active');
-  }
+// Add event listeners to each label
+label1.addEventListener('click', function() {
+  alert('Label 1 Clicked!');
 });
 
-overlay.addEventListener('mouseout', () => {
-  // Hide both labels when the mouse moves away from the overlay
-  label1.classList.remove('active');
-  label2.classList.remove('active');
+label2.addEventListener('click', function() {
+  alert('Label 2 Clicked!');
 });
