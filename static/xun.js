ebdf9618c4652c59267b766c7e559cadc0a1f7af
Derek Lorenzo <87837145+DerekLorenzo@users.noreply.github.com>
2023-05-12 05:55:56 -0400

    update xun sounds

--- static/xun.js
+++ static/xun.js
@@ -1,6 +1,10 @@
 // Load the audio files
-var sideSound = new Audio('sounds/side-bell.mp3');
-var centerSound = new Audio('sounds/center-bell.mp3');
+var firstSound = new Audio('sounds/xun-0.mp3');
+var secondSound = new Audio('sounds/xun-1.mp3');
+var thirdSound = new Audio('sounds/xun-2.mp3');
+var fourthSound = new Audio('sounds/xun-3.mp3');
+var fifthSound = new Audio('sounds/xun-4.mp3');
+var sixthSound = new Audio('sounds/xun-5.mp3');
 
 // Get the bell image and clickable areas
 var bell = document.getElementById('bell');
@@ -13,30 +17,30 @@ var bottomRightArea = document.querySelector('.bottom-right-area');
 
 topLeftArea.addEventListener('click', function () {
     // Play the center bell sound
-    centerSound.play();
+   thirdSound.play();
 });
 
 topRightArea.addEventListener('click', function () {
     // Play the center bell sound
-    sideSound.play();
+    sixthSound.play();
 });
 
 middleLeftArea.addEventListener('click', function () {
     // Play the center bell sound
-    centerSound.play();
+    secondSound.play();
 });
 
 middleRightArea.addEventListener('click', function () {
     // Play the center bell sound
-    sideSound.play();
+    fifthSound.play();
 });
 
 bottomLeftArea.addEventListener('click', function () {
     // Play the center bell sound
-    centerSound.play();
+    firstSound.play();
 });
 
 bottomRightArea.addEventListener('click', function () {
     // Play the center bell sound
-    sideSound.play();
+    fourthSound.play();
 });
