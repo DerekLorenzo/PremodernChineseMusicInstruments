b012ca4e35dbcf3d3e8ffc7021b0fa83aa68be23
Derek Lorenzo <87837145+DerekLorenzo@users.noreply.github.com>
2023-05-12 02:16:09 -0400

    update labels

--- static/pipa.js
+++ static/pipa.js
@@ -1,12 +1,23 @@
-// Change these variables to match your label IDs and text
-const label1 = document.getElementById('label-1');
-const label2 = document.getElementById('label-2');
+// Get the instrument image element and all label elements
+const instrumentImage = document.getElementById("pipa");
+const labels = document.querySelectorAll(".label");
 
-// Add event listeners to each label
-label1.addEventListener('click', function() {
-  alert('Label 1 Clicked!');
-});
+// Loop through each label element and add a click event listener to it
+labels.forEach((label) => {
+  const point = document.createElement("div");
+  point.classList.add("point");
+  instrumentImage.appendChild(point);
 
-label2.addEventListener('click', function() {
-  alert('Label 2 Clicked!');
+  point.style.top = label.dataset.top + "px";
+  point.style.left = label.dataset.left + "px";
+  
+  point.addEventListener("click", () => {
+    if (label.style.display === "none") {
+      // Show the label if it's hidden
+      label.style.display = "block";
+    } else {
+      // Hide the label if it's visible
+      label.style.display = "none";
+    }
+  });
 });
