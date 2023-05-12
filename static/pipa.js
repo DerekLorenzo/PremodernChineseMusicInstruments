e6b3dbb438e0506512467b2ce10b1fc7288bab4d
Derek Lorenzo <87837145+DerekLorenzo@users.noreply.github.com>
2023-05-12 01:47:20 -0400

    added overlay

--- static/pipa.js
+++ static/pipa.js
@@ -1,7 +1,20 @@
-const labels = document.querySelectorAll('.label');
+// Get the label and overlay elements
+const label1 = document.getElementById('label1');
+const label2 = document.getElementById('label2');
+const overlay = document.getElementById('overlay');
 
-labels.forEach((label) => {
-  label.addEventListener('click', () => {
-    label.classList.toggle('active');
-  });
+// Add event listeners to the overlay
+overlay.addEventListener('mouseover', () => {
+  // Show the appropriate label based on which part of the overlay was clicked
+  if (event.offsetX < overlay.offsetWidth / 2) {
+    label1.classList.add('active');
+  } else {
+    label2.classList.add('active');
+  }
+});
+
+overlay.addEventListener('mouseout', () => {
+  // Hide both labels when the mouse moves away from the overlay
+  label1.classList.remove('active');
+  label2.classList.remove('active');
 });
